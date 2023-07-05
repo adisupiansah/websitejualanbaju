47421350bc3f15c1f2ba580e0c04310967bd8f1a
kuizteknikinformatika <adisupiansah15@gmail.com>
2023-07-06 02:35:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const nonton = document.getElementById('nonton');
+
+nonton.addEventListener('click', function () {
+    swal ({
+        title: 'IIIH SANGE LU YA HAHAHAH',
+        text: 'mampus lu di prank',
+        icon: './assets/gambar.png',
+        button: 'ya saya cabul',
+
+    });
+});
+
+const nonton2 = document.getElementById('nonton2');
+
+nonton2.addEventListener('click', function () {
+    swal ({
+        title: 'IIIH SANGE LU YA HAHAHAH',
+        text: 'mampus lu di prank',
+        icon: './assets/gambar.png',
+        button: 'ya saya cabul',
+
+    });
+});
+
+const nonton3 = document.getElementById('nonton3');
+
+nonton3.addEventListener('click', function () {
+    swal ({
+        title: 'IIIH SANGE LU YA HAHAHAH',
+        text: 'mampus lu di prank',
+        icon: './assets/gambar.png',
+        button: 'ya saya cabul',
+
+    });
+});
+
